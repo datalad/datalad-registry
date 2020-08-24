784bca7e6a94f3923e2ccdfb01c69c0ee6550c66
Kyle Meyer <kyle@kyleam.com>
2020-08-24 16:28:57 -0400

    Initial database setup

--- datalad_registry/__init__.py
+++ datalad_registry/__init__.py
@@ -5,11 +5,15 @@ from pathlib import Path
 from flask import Flask
 
 from datalad_registry import dataset_urls
+from datalad_registry import db
 
 
 def create_app():
     app = Flask(__name__)
     instance_path = Path(app.instance_path)
+    app.config.from_mapping(
+        DATABASE=str(instance_path / "registry.sqlite"))
     instance_path.mkdir(parents=True, exist_ok=True)
+    db.init_app(app)
     app.register_blueprint(dataset_urls.bp)
     return app
