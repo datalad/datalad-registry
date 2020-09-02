9b84bef50ef71e22dac4a86793500d463e684a1e
Kyle Meyer <kyle@kyleam.com>
2020-09-02 15:40:16 -0400

    Initial flask setup

--- datalad_registry/__init__.py
+++ datalad_registry/__init__.py
@@ -1,1 +1,12 @@
 __version__ = "0.0.0"
+
+from pathlib import Path
+
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    instance_path = Path(app.instance_path)
+    instance_path.mkdir(parents=True, exist_ok=True)
+    return app
