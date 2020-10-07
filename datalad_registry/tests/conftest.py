f52ac306abc745776b3692ef8cb3863d5d8010f6
Kyle Meyer <kyle@kyleam.com>
2020-10-07 15:01:19 -0400

    tests: Add fixture that provides access to app and db
    The client fixture only exposes the client, but upcoming tests will
    need access to db and app.  Add a fixture that exposes these details,
    and rework the client fixture to use it.

--- datalad_registry/tests/conftest.py
+++ datalad_registry/tests/conftest.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import uuid
 from random import Random
 
@@ -9,6 +10,8 @@ from datalad_registry.models import db
 random = Random()
 random.seed("datalad-registry")
 
+AppInstance = namedtuple("AppInstance", ["app", "db", "client"])
+
 
 @pytest.fixture
 def dsid():
@@ -16,7 +19,16 @@ def dsid():
 
 
 @pytest.fixture
-def client():
+def app_instance():
+    """Fixture that provides the application, database, and client.
+
+    If you just need the client, you can use the `client` fixture
+    instead.
+
+    Yields
+    ------
+    AppInstance namedtuple with app, db, and client fields.
+    """
     config = {"CELERY_BEAT_SCHEDULE": {},
               "CELERY_TASK_ALWAYS_EAGER": True,
               "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
@@ -25,4 +37,14 @@ def client():
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
-        yield client
+        yield AppInstance(app, db, client)
+
+
+@pytest.fixture
+def client(app_instance):
+    """Fixture that provides client.
+
+    If you need to access to the application or database, use the
+    `app_instance` fixture instead.
+    """
+    yield app_instance.client
