50ebf2ae21eb0e9ac2796b83ed0e3b548fc447a4
Kyle Meyer <kyle@kyleam.com>
2021-01-14 17:34:33 -0500

    views: Paginate the /datasets response
    Go with 100 items per page as an uninformed guess at a sensible
    number.

--- datalad_registry/tests/test_datasets.py
+++ datalad_registry/tests/test_datasets.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from datalad_registry.utils import url_encode
 from datalad_registry.tests.utils import init_repo_with_token
 from datalad_registry.tests.utils import make_dsid
@@ -25,4 +27,17 @@ def test_datasets(client, tmp_path):
     dsids = sorted(dsids)
 
     r_datasets = client.get("/v1/datasets").get_json()
+    assert r_datasets["next"] is None
+    assert r_datasets["previous"] is None
     assert dsids == r_datasets["dsids"]
+
+    with patch("datalad_registry.datasets._PAGE_NITEMS", 3):
+        r_datasets_pg1 = client.get("/v1/datasets").get_json()
+        assert r_datasets_pg1["next"] == "/v1/datasets?page=2"
+        assert r_datasets_pg1["previous"] is None
+        assert dsids[:3] == r_datasets_pg1["dsids"]
+
+        r_datasets_pg2 = client.get(r_datasets_pg1["next"]).get_json()
+        assert r_datasets_pg2["next"] is None
+        assert r_datasets_pg2["previous"] == "/v1/datasets?page=1"
+        assert dsids[3:] == r_datasets_pg2["dsids"]
