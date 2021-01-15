275561b075a4d6a553b0f5f7a6610ae2b56c6035
Kyle Meyer <kyle@kyleam.com>
2021-01-15 11:10:44 -0500

    tests: Add init_repo_with_token helper
    test_register_url() doesn't need a proper DataLad dataset, so it sets
    up a lightweight plain git repo with an empty tree and a single
    commit, along with the token ref.  An upcoming test will do the same,
    so add a helper.

--- datalad_registry/tests/utils.py
+++ datalad_registry/tests/utils.py
@@ -1,3 +1,4 @@
+import subprocess as sp
 import uuid
 from random import Random
 
@@ -12,3 +13,9 @@ def make_dsid():
     full-fledged datasets.
     """
     return str(uuid.UUID(int=random.getrandbits(128)))
+
+
+def init_repo_with_token(path, token_response):
+    sp.run(["git", "init"], cwd=path)
+    sp.run(["git", "commit", "--allow-empty", "-mc0"], cwd=path)
+    sp.run(["git", "update-ref", token_response["ref"], "HEAD"], cwd=path)
