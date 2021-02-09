2a56c3e952259b83b0ef8624229af3e236357d56
Kyle Meyer <kyle@kyleam.com>
2021-02-09 16:48:46 -0500

    tests: Document init_repo_with_token
    In particular, explicitly state its purpose.

--- datalad_registry/tests/utils.py
+++ datalad_registry/tests/utils.py
@@ -16,6 +16,20 @@ def make_ds_id():
 
 
 def init_repo_with_token(path, token_response):
+    """Initialize empty repo with a challenge reference.
+
+    This creates a minimal repository suitable for tests that don't
+    need a proper dataset.
+
+    Parameters
+    ----------
+    path : str
+        Initialize repository at this location.
+    token_response : dict
+        A response from the token endpoint.  The value of its "ref"
+        key is used as the challenge reference.
+    """
+    # Note: DataLad is intentionally avoided here.
     sp.run(["git", "init"], cwd=path)
     sp.run(["git", "commit", "--allow-empty", "-mc0"], cwd=path)
     sp.run(["git", "update-ref", token_response["ref"], "HEAD"], cwd=path)
