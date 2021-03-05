9ce13942997e13646e439df43941b19a8bf8db95
Kyle Meyer <kyle@kyleam.com>
2021-03-05 17:55:40 -0500

    client: Add command for announcing update

--- datalad_registry_client/__init__.py
+++ datalad_registry_client/__init__.py
@@ -3,4 +3,8 @@ command_suite = ("Interact with DataLad registry",
                  [("datalad_registry_client.submit",
                    "RegistrySubmit",
                    "registry-submit",
-                   "registry_submit")])
+                   "registry_submit"),
+                  ("datalad_registry_client.announce_update",
+                   "RegistryAnnounceUpdate",
+                   "registry-announce-update",
+                   "registry_announce_update")])
