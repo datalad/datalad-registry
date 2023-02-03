c1238386c5c0cd569d3b3be7a72b3c7cefed8632
Isaac To <candleindark@gmail.com>
2023-02-03 11:44:42 -0800

    Define model for representing GitHub repos on dashboard

--- datalad_registry/tests/populate.py
+++ datalad_registry/tests/populate.py
@@ -1,6 +1,36 @@
 # Warning: This script populates the running datalad-registry instance with selected
 #          datasets from https://github.com/datalad/datalad-usage-dashboard/.
 
+from pydantic import BaseModel, HttpUrl, StrictBool, StrictInt, StrictStr
+
+from datalad_registry.utils import StrEnum
+
+
+class Status(StrEnum):
+    """
+    Enum for representing the status of repo
+    """
+
+    active = "active"
+    gone = "gone"
+
+
+class GitHubRepo(BaseModel):
+    """
+    Pydantic model for representing GitHub repository information found in
+    the datalad-usage-dashboard repository
+    """
+
+    id: StrictInt
+    name: StrictStr
+    url: HttpUrl
+    stars: StrictInt
+    dataset: StrictBool
+    run: StrictBool
+    container_run: StrictBool
+    status: Status
+
+
 # Establish a Pydantic model for representing dataset information obtain from
 # the datalad-usage-dashboard repository
 
