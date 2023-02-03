d1b91d4cb5df34d9c8a3f115bfb27a627bead94e
Isaac To <candleindark@gmail.com>
2023-02-03 10:41:25 -0800

    Outline the procedure in populate.py

--- datalad_registry/tests/populate.py
+++ datalad_registry/tests/populate.py
@@ -1,2 +1,9 @@
 # Warning: This script populates the running datalad-registry instance with selected
 #          datasets from https://github.com/datalad/datalad-usage-dashboard/.
+
+# Establish a Pydantic model for representing dataset information obtain from
+# the datalad-usage-dashboard repository
+
+# Fetch dataset information from the datalad-usage-dashboard repository
+
+# Submit dataset urls to the datalad-registry instance at once or sequentially
