8c963a068a1b74b7dae2a00c75e69d95beb14754
Isaac To <candleindark@gmail.com>
2023-03-01 08:16:18 -0800

    Establish model for metadata of URL

--- datalad_registry/models.py
+++ datalad_registry/models.py
@@ -25,8 +25,36 @@ class URL(db.Model):  # type: ignore
     #: Whether initial data has been collected for this URL
     processed = db.Column(db.Boolean, default=False, nullable=False)
 
+    meta_data = db.relationship("URLMetadata", backref="url")
+
     def __repr__(self) -> str:
         return f"<URL(url={self.url!r}, ds_id={self.ds_id!r})>"
+
+
+class URLMetadata(db.Model):  # type: ignore
+    """
+    Model for dataset level metadata of a dataset at a specific URL.
+    """
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+
+    # The head_describe of the dataset at the associated URL at the time of extraction
+    dataset_describe = db.Column(db.String(60), nullable=False)
+
+    dataset_version = db.Column(db.String(60), nullable=False)
+    extractor_name = db.Column(db.String(100), nullable=False)
+    extractor_version = db.Column(db.String(60), nullable=False)
+    extraction_parameter = db.Column(db.JSON, nullable=False)
+
+    extracted_metadata = db.Column(db.JSON, nullable=False)
+
+    # The ID of the associated URL
+    url_id = db.Column(db.Integer, db.ForeignKey("url.id"), nullable=False)
+
+    def __repr__(self) -> str:
+        return (
+            f"<URLMetadata(url={self.url.url!r}, extractor={self.extractor_name!r})> "
+        )
 
 
 @click.command("init-db")
