9066fd4a04af9035bcfd42ea6697b4d261eb76b7
Isaac To <candleindark@gmail.com>
2023-03-07 15:51:11 -0800

    Define URLMetadataModel
    For communication of URLMetadata in JSON

--- datalad_registry/com_models.py
+++ datalad_registry/com_models.py
@@ -2,7 +2,7 @@
 # of the application.
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, StrictStr
 
 
 class MetadataRecord(BaseModel):
@@ -20,3 +20,20 @@ class MetaExtractResult(BaseModel):
     action: Literal["meta_extract"]
     status: str
     metadata_record: MetadataRecord
+
+
+class URLMetadataModel(BaseModel):
+    """
+    Pydantic model for representing the database model URLMetadata for communication
+    in JSON
+    """
+
+    dataset_describe: StrictStr
+    dataset_version: StrictStr
+    extractor_name: StrictStr
+    extractor_version: StrictStr
+    extraction_parameter: dict
+    extracted_metadata: dict
+
+    class Config:
+        orm_mode = True
