531814c4ea69c973623cd3f979ff4250d8b1fd8c
Isaac To <candleindark@gmail.com>
2023-03-25 13:06:59 -0700

    Paginate without args and pass pagination obj to template directly
    paginate() defaults its args to the arguments of the request.
    There is really no needed pass argument manually. Additionally,
    without passing argument manually, a user can set pagination
    parameters through the request args in URL easily.
    The resulting pagination object is passed directly,
    and the unnecessary `rows` object is eliminated.

--- datalad_registry/overview.py
+++ datalad_registry/overview.py
@@ -11,7 +11,6 @@ from datalad_registry.models import URL, db
 lgr = logging.getLogger(__name__)
 bp = Blueprint("overview", __name__, url_prefix="/overview")
 
-_PAGE_NITEMS = 25  # TODO: Should eventually be configurable by app.
 _SORT_ATTRS = {
     "keys-asc": ("annex_key_count", "asc"),
     "keys-desc": ("annex_key_count", "desc"),
@@ -26,19 +25,6 @@ _SORT_ATTRS = {
     "git_objects_kb-asc": ("git_objects_kb", "asc"),
     "git_objects_kb-desc": ("git_objects_kb", "desc"),
 }
-
-# Columns of the table displayed on the overview page
-_COLS = [
-    "ds_id",
-    "url",
-    "annex_key_count",
-    "head",
-    "head_describe",
-    "annexed_files_in_wt_count",
-    "annexed_files_in_wt_size",
-    "git_objects_kb",
-    "metadata_",
-]
 
 
 @bp.get("/")
@@ -62,32 +48,12 @@ def overview():  # No type hints due to mypy#7187.
     col, sort_method = _SORT_ATTRS[sort_by]
     r = r.order_by(nullslast(getattr(getattr(URL, col), sort_method)()))
 
-    # Get total number of URLs to be displayed through pagination
-    num_urls = r.count()
-
     # Paginate
-    page = request.args.get("page", 1, type=int)
-    r = r.paginate(page=page, per_page=_PAGE_NITEMS, error_out=False)
-
-    # Generate rows
-    rows = []
-    for item in r.items:
-        row = {col: getattr(item, col) for col in _COLS}
-
-        ts = item.info_ts
-        if ts is not None:
-            row["last_update"] = ts.strftime("%Y-%m-%dT%H:%M:%S%z")
-        else:
-            row["last_update"] = None
-        rows.append(row)
+    pagination = r.paginate()
 
     return render_template(
         "overview.html",
-        rows=rows,
-        page=page,
-        has_next=r.has_next,
-        has_prev=r.has_prev,
+        pagination=pagination,
         sort_by=sort_by,
         url_filter=url_filter,
-        num_urls=num_urls,
     )
