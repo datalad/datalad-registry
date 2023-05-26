2bc0e74c813c36d99031ddf89edbd70538708657
Isaac To <candleindark@gmail.com>
2023-05-26 16:09:13 -0700

    Modify docstring

--- datalad_registry_client/tests/test_get_urls.py
+++ datalad_registry_client/tests/test_get_urls.py
@@ -3,8 +3,7 @@ from datalad.tests.utils_pytest import assert_result_count
 
 def test_register():
     """
-    Test that `registry_get_urls` is registered with DataLad.
-    :return:
+    Test that `registry_get_urls` is registered with DataLad
     """
     import datalad.api as ds
 
