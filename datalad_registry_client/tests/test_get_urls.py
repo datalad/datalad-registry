dd22c93ca08b63c6899f5e3f3971daefdda1651a
Isaac To <candleindark@gmail.com>
2023-05-26 16:09:55 -0700

    Remove unneeded call of the command

--- datalad_registry_client/tests/test_get_urls.py
+++ datalad_registry_client/tests/test_get_urls.py
@@ -1,6 +1,3 @@
-from datalad.tests.utils_pytest import assert_result_count
-
-
 def test_register():
     """
     Test that `registry_get_urls` is registered with DataLad
@@ -8,4 +5,3 @@ def test_register():
     import datalad.api as ds
 
     assert hasattr(ds, "registry_get_urls")
-    assert_result_count(ds.registry_get_urls(), 1, action="registry-get-urls")
