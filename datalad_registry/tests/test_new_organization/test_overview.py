89b5c9e6a1465e4d0f284dcbdcc3ae78009f6a4f
Isaac To <candleindark@gmail.com>
2023-06-18 14:19:08 -0700

    Outline new tests for the web UI

---
+++ datalad_registry/tests/test_new_organization/test_overview.py
@@ -0,0 +1,15 @@
+class TestOverView:
+    def test_sorting(self):
+        pass
+
+    def test_filter(self):
+        pass
+
+    def test_pagination(self):
+        pass
+
+    def test_metadata(self):
+        """
+        Test for the present of metadata in the overview page
+        """
+        pass
