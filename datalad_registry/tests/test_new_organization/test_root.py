f7c7df231d1555839bec570a2c1c5e5c96abdd8f
Isaac To <candleindark@gmail.com>
2023-06-18 21:36:33 -0700

    Remove outdated tests and its support

--- datalad_registry/tests/test_new_organization/test_root.py
+++ datalad_registry/tests/test_new_organization/test_root.py
@@ -1,4 +1,4 @@
-def test_root_redirect(client):
-    r_root = client.get("/")
+def test_root_redirect(flask_client):
+    r_root = flask_client.get("/")
     assert r_root.status_code == 302
     assert r_root.headers["Location"].endswith("/overview/")
