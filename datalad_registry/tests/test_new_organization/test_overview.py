57a9b25ebb999ff9c3b5b4fabf343e04349b8ec0
Isaac To <candleindark@gmail.com>
2023-06-18 19:59:31 -0700

    Add test for pagination in web UI

--- datalad_registry/tests/test_new_organization/test_overview.py
+++ datalad_registry/tests/test_new_organization/test_overview.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from bs4 import BeautifulSoup
 import pytest
+from yarl import URL as YURL
 
 
 class TestOverView:
@@ -173,8 +174,73 @@ class TestOverView:
 
         assert url_list == expected_results
 
-    def test_pagination(self):
-        pass
+    def test_pagination(self, populate_with_dataset_urls, flask_client):
+        """
+        Test pagination in Web UI
+        """
+
+        # For storing all dataset URL obtained from all pages
+        ds_urls: set[str] = set()
+
+        # === Get the first page ===
+        resp = flask_client.get("/overview/", query_string={"per_page": 2})
+
+        # noinspection DuplicatedCode
+        assert resp.status_code == 200
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        # Get all dataset URLs from the first page
+        page_ds_urls = [row.td.a.string for row in soup.body.table.find_all("tr")[1:]]
+        assert len(page_ds_urls) == 2
+
+        # Store all dataset URLs from the first page
+        for url in page_ds_urls:
+            ds_urls.add(url)
+
+        pagination_element = soup.find("div", {"class": "pagination"})
+        page_1_element = pagination_element.strong
+        page_2_element = pagination_element.a
+        page_2_link = YURL(page_2_element["href"])
+
+        assert page_1_element.string == "1"
+        assert page_2_element.string == "2"
+        assert page_2_link.path == "/overview/"
+        assert len(page_2_link.query) == 3
+        assert page_2_link.query["page"] == "2"
+        assert page_2_link.query["per_page"] == "2"
+        assert page_2_link.query["sort"] == "update-desc"
+
+        # === Get the second page ===
+        resp = flask_client.get(str(page_2_link))
+
+        # noinspection DuplicatedCode
+        assert resp.status_code == 200
+
+        soup = BeautifulSoup(resp.text, "html.parser")
+
+        # Get all dataset URLs from the second page
+        page_ds_urls = [row.td.a.string for row in soup.body.table.find_all("tr")[1:]]
+        assert len(page_ds_urls) == 2
+
+        # Store all dataset URLs from the second page
+        for url in page_ds_urls:
+            ds_urls.add(url)
+
+        pagination_element = soup.find("div", {"class": "pagination"})
+        page_1_element = pagination_element.a
+        page_2_element = pagination_element.strong
+        page_1_link = YURL(page_1_element["href"])
+
+        assert page_1_element.string == "1"
+        assert page_2_element.string == "2"
+        assert page_1_link.path == "/overview/"
+        assert len(page_1_link.query) == 3
+        assert page_1_link.query["page"] == "1"
+        assert page_1_link.query["per_page"] == "2"
+        assert page_1_link.query["sort"] == "update-desc"
+
+        assert ds_urls == set(populate_with_dataset_urls)
 
     @pytest.mark.usefixtures("populate_with_url_metadata")
     def test_metadata(self, flask_client):
