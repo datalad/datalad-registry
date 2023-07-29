099ce7cfd20f5899bea4d579f04251c2ddb220bf
Isaac To <candleindark@gmail.com>
2023-07-29 11:10:20 -0700

    Turn on case sensitivity for reading of env vars

--- datalad_registry/conf.py
+++ datalad_registry/conf.py
@@ -4,6 +4,9 @@ from pydantic import BaseSettings
 class DefaultConfig(BaseSettings):
     CELERY = {}
 
+    class Config:
+        case_sensitive = True
+
 
 class ProductionConfig(DefaultConfig):
     pass
