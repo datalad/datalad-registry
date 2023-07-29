70856e2dcc4e1f7d64620b7c46e79a57b6a04c2d
Isaac To <candleindark@gmail.com>
2023-07-29 11:31:34 -0700

    Provide option for mode of operation

--- datalad_registry/conf.py
+++ datalad_registry/conf.py
@@ -1,7 +1,18 @@
+from enum import auto
+
 from pydantic import BaseSettings
+
+from datalad_registry.utils.misc import StrEnum
+
+
+class OperationMode(StrEnum):
+    PRODUCTION = auto()
+    DEVELOPMENT = auto()
+    TESTING = auto()
 
 
 class DefaultConfig(BaseSettings):
+    DATALAD_REGISTRY_OPERATION_MODE: OperationMode = OperationMode.PRODUCTION
     CELERY = {}
 
     class Config:
