ea45943f2b6ebc541595c772b5fb03edeb10f964
Isaac To <candleindark@gmail.com>
2023-07-30 14:02:08 -0700

    Enable `BaseConfig` to fetch Celery related configs from env directly

--- datalad_registry/conf.py
+++ datalad_registry/conf.py
@@ -1,5 +1,6 @@
 from enum import auto
 from pathlib import Path
+from typing import Any
 
 from pydantic import BaseSettings, validator
 
@@ -20,7 +21,19 @@ class BaseConfig(BaseSettings):
     DATALAD_REGISTRY_INSTANCE_PATH: Path
     DATALAD_REGISTRY_DATASET_CACHE: Path
 
-    CELERY: dict
+    # Celery related configuration
+    CELERY_BROKER_URL: str
+    CELERY_RESULT_BACKEND: str
+    CELERY_TASK_IGNORE_RESULT: bool
+
+    # noinspection PyPep8Naming
+    @property
+    def CELERY(self) -> dict[str, Any]:
+        return dict(
+            broker_url=self.CELERY_BROKER_URL,
+            result_backend=self.CELERY_RESULT_BACKEND,
+            task_ignore_result=self.CELERY_TASK_IGNORE_RESULT,
+        )
 
     _path_must_be_absolute = validator(
         "DATALAD_REGISTRY_INSTANCE_PATH",
@@ -33,23 +46,15 @@ class BaseConfig(BaseSettings):
 
 
 class ProductionConfig(BaseConfig):
-    CELERY = dict(
-        broker_url="",  # TODO: to be specified
-        result_backend="",  # TODO: to be specified
-        task_ignore_result=True,
-    )
+    CELERY_TASK_IGNORE_RESULT: bool = True
 
 
 class DevelopmentConfig(BaseConfig):
-    CELERY = dict(
-        broker_url="",  # TODO: to be specified
-        result_backend="",  # TODO: to be specified
-        task_ignore_result=True,
-    )
+    CELERY_TASK_IGNORE_RESULT: bool = True
 
 
 class TestingConfig(BaseConfig):
-    pass
+    CELERY_TASK_IGNORE_RESULT: bool = True
 
 
 class ReadOnlyConfig(BaseConfig):
@@ -63,4 +68,8 @@ class ReadOnlyConfig(BaseConfig):
     # The following are just dummy values to serve as defaults to satisfy
     # the configuration requirements and make the corresponding fields optional.
     DATALAD_REGISTRY_DATASET_CACHE: Path = Path("/dummy/path")
+
+    CELERY_BROKER_URL: str = "dummy://"
+    CELERY_RESULT_BACKEND: str = "dummy://"
+    CELERY_TASK_IGNORE_RESULT: bool = True
     CELERY = {}
