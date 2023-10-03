6374c1afec2f9b6dc1f2b1557897a60a07f550dd
Isaac To <candleindark@gmail.com>
2023-10-02 19:25:47 -0700

    Add test for `chk_url_to_update`
    Against the case that the check to update the dataset
    at the given URL is already handled by another process

--- datalad_registry/tests/test_tasks/test_chk_url_to_update.py
+++ datalad_registry/tests/test_tasks/test_chk_url_to_update.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 import pytest
 
 from datalad_registry.tasks import ChkUrlStatus, chk_url_to_update
@@ -14,3 +16,20 @@ class TestChkUrlToUpdate:
         Test the case that no RepoUrl record with the given ID exists in the database
         """
         assert chk_url_to_update(invalid_url_id, None) is ChkUrlStatus.ABORTED
+
+    @pytest.mark.usefixtures("populate_with_dataset_urls")
+    @pytest.mark.parametrize(
+        "url_id, initial_last_chk_dt",
+        [
+            (1, None),
+            (2, datetime(2009, 6, 18, 18, 34, 31, tzinfo=timezone.utc)),
+            (3, datetime(2004, 6, 18, 19, 33, 7, tzinfo=timezone.utc)),
+        ],
+    )
+    def test_chk_handled_by_another_process(self, url_id, initial_last_chk_dt):
+        """
+        Test the case that the check to update the dataset at the given URL is
+        already handled by another process (signaled by having a modified `last_chk_dt`
+        in the `RepoUrl` record)
+        """
+        assert chk_url_to_update(url_id, initial_last_chk_dt) is ChkUrlStatus.SKIPPED
