37bb785ba6764614f0141800936abced3a41c811
Isaac To <candleindark@gmail.com>
2023-10-09 11:12:49 -0700

    Define `disable_in_read_only_mode` decorator function

--- datalad_registry/utils/flask_tools.py
+++ datalad_registry/utils/flask_tools.py
@@ -1,7 +1,10 @@
+from functools import wraps
 from http import HTTPStatus
 from typing import Optional, Union
 
-from flask import current_app
+from flask import abort, current_app
+
+from datalad_registry.conf import OperationMode
 
 
 def json_resp_from_str(
@@ -19,3 +22,27 @@ def json_resp_from_str(
     return current_app.response_class(
         json_str, mimetype="application/json", status=status
     )
+
+
+def disable_in_read_only_mode(view_func):
+    """
+    Decorator for view functions that should be disabled in read-only mode
+
+    :param view_func: The view function to decorate
+    :return: The decorated view function
+    """
+
+    @wraps(view_func)
+    def wrapper(*args, **kwargs):
+        if (
+            current_app.config["DATALAD_REGISTRY_OPERATION_MODE"]
+            is OperationMode.READ_ONLY
+        ):
+            abort(
+                HTTPStatus.IM_A_TEAPOT,
+                description="This operation is not available from a read-only server.",
+            )
+        else:
+            return view_func(*args, **kwargs)
+
+    return wrapper
