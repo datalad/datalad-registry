c78d7ed63a6ec085680c31707bec53e519933c92
Isaac To <candleindark@gmail.com>
2023-10-12 22:09:26 -0700

    Outline the implementation of DANDI extractors

---
+++ datalad_registry/tasks/utils/builtin_meta_extractors.py
@@ -0,0 +1,59 @@
+# This file specifies custom metadata extractors, for datalad_registry, and related
+# definitions.
+from collections.abc import Callable
+
+from datalad_registry.models import RepoUrl, URLMetadata
+
+
+def dlreg_dandiset_meta_extract(url: RepoUrl) -> URLMetadata:
+    """
+    Extract the metadata specified in the `dandiset.yaml` file of the DANDI dataset
+    at a given URL
+
+    :param url: The `RepoUrl` object representing the URL
+                at which the dataset is located
+    :return: A `URLMetadata` object containing the extracted metadata ready
+             to be written (committed) to the database
+
+    Note: This function implements the `dandi:dandiset` extractor.
+    """
+    raise NotImplementedError
+
+
+def dlreg_dandi_assets_meta_extract(url: RepoUrl) -> URLMetadata:
+    """
+    Extract the metadata specified in the `.dandi/assets.json` file of the DANDI dataset
+    at a given URL
+
+    :param url: The `RepoUrl` object representing the URL
+    :return: A `URLMetadata` object containing the extracted metadata ready
+
+    Note: This function implements the `dandi:assets` extractor.
+    """
+    raise NotImplementedError
+
+
+# A mapping from the names of the supported extractors to the functions
+# that implement those extractors respectively
+EXTRACTOR_MAP: dict[str, Callable[[RepoUrl], URLMetadata]] = {
+    "dandi:dandiset": dlreg_dandiset_meta_extract,
+    "dandi:assets": dlreg_dandi_assets_meta_extract,
+}
+
+
+def dlreg_meta_extract(extractor: str, url: RepoUrl) -> URLMetadata:
+    """
+    Extract metadata from the dataset at a given URL using the specified extractor.
+
+    :param extractor: The name of the extractor to use
+    :param url: The `RepoUrl` object representing the URL
+                at which the dataset is located
+    :return: A `URLMetadata` object containing the extracted metadata ready
+             to be written (committed) to the database
+    :raises ValueError: If the argument for `extractor` is not one of the extractors
+                        specified in `SUPPORTED_EXTRACTORS`
+    """
+    if extractor not in EXTRACTOR_MAP:
+        raise ValueError(f"Extractor {extractor} not supported")
+
+    return EXTRACTOR_MAP[extractor](url)
