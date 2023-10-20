653f2b2a83c9f7bde972dfcadb4ce0f1e17c86c2
Isaac To <candleindark@gmail.com>
2023-10-19 18:07:46 -0700

    Provide tests for the log filter

--- datalad_registry/tests/test_make_celery.py
+++ datalad_registry/tests/test_make_celery.py
@@ -1,3 +1,5 @@
+import logging
+
 from celery import Celery
 import pytest
 
@@ -11,3 +13,51 @@ def test_celery_app_instantiation():
 
     assert celery_app is flask_app.extensions["celery"]
     assert isinstance(celery_app, Celery)
+
+
+class TestSuppressKnownGitProgressReport:
+    # Set needed environment to instantiate a flask app in datalad_registry.make_celery
+    @pytest.mark.usefixtures("set_test_env")
+    @pytest.mark.parametrize(
+        "msg, expected_result",
+        [
+            ("hello", True),
+            ("Start receiving objects", True),
+            ("Start counting objects", True),
+            (
+                "Failed to get 'sourcedata/templateflow/tpl-NKI.path', "
+                "skipping this submodule",
+                True,
+            ),
+            (
+                "Failed to get 'sourcedata/templateflow/tpl-WHS.path', "
+                "skipping this submodule",
+                True,
+            ),
+            ("Finished enumerating objects", True),
+            ("Finished compressing objects", True),
+            ("Resolving deltas:  65% (19502/30003)", False),
+            ("Receiving objects:   2% (8943/447132)", False),
+            ("Receiving objects:  57% (194348/340960), 9.80 MiB | 9.79 MiB/s", False),
+            ("remote: Compressing objects: 100% (111150/111150), done.", False),
+            ("remote: Compressing objects:  89% (98924/111150)", False),
+            ("Resolving deltas:  90% (27003/30003)", False),
+        ],
+    )
+    def test_filter(self, msg, expected_result):
+        """
+        Test the filter method of the SuppressKnownGitProgressReport class
+        """
+        from datalad_registry.make_celery import SuppressKnownGitProgressReport
+
+        record = logging.LogRecord(
+            name="logger",
+            level=logging.DEBUG,
+            pathname="",
+            lineno=1,
+            msg=msg,
+            args=(),
+            exc_info=None,
+        )
+
+        assert SuppressKnownGitProgressReport().filter(record) is expected_result
