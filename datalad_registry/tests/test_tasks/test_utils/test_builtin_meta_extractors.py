17c5ac43a23ab2b27dd457a7c37b7760669420e7
Isaac To <candleindark@gmail.com>
2023-10-20 18:40:38 -0700

    Move the test for `dlreg_dandiset_meta_extract` into a class

--- datalad_registry/tests/test_tasks/test_utils/test_builtin_meta_extractors.py
+++ datalad_registry/tests/test_tasks/test_utils/test_builtin_meta_extractors.py
@@ -7,20 +7,25 @@ from datalad_registry.tasks.utils.builtin_meta_extractors import (
 from datalad_registry.utils.datalad_tls import get_head_describe
 
 
-def test_dlreg_dandiset_meta_extract(dandi_repo_url_with_up_to_date_clone, flask_app):
-    repo_url = dandi_repo_url_with_up_to_date_clone[0]
-    ds_clone = dandi_repo_url_with_up_to_date_clone[2]
+class TestDlregDandisetMetaExtract:
+    def test_valid_input(self, dandi_repo_url_with_up_to_date_clone, flask_app):
+        """
+        Test the case that the argument `url` is a valid `RepoUrl` object with a
+        valid corresponding dandi dataset in the local cache
+        """
+        repo_url = dandi_repo_url_with_up_to_date_clone[0]
+        ds_clone = dandi_repo_url_with_up_to_date_clone[2]
 
-    with flask_app.app_context():
-        url_metadata = dlreg_dandiset_meta_extract(repo_url)
+        with flask_app.app_context():
+            url_metadata = dlreg_dandiset_meta_extract(repo_url)
 
-    assert url_metadata.dataset_describe == get_head_describe(ds_clone)
-    assert url_metadata.dataset_version == ds_clone.repo.get_hexsha()
-    assert url_metadata.extractor_name == "dandi:dandiset"
-    assert url_metadata.extractor_version == "0.0.1"
-    assert url_metadata.extraction_parameter == {}
-    assert url_metadata.extracted_metadata == {"name": "test-dandi-ds"}
-    assert url_metadata.url == repo_url
+        assert url_metadata.dataset_describe == get_head_describe(ds_clone)
+        assert url_metadata.dataset_version == ds_clone.repo.get_hexsha()
+        assert url_metadata.extractor_name == "dandi:dandiset"
+        assert url_metadata.extractor_version == "0.0.1"
+        assert url_metadata.extraction_parameter == {}
+        assert url_metadata.extracted_metadata == {"name": "test-dandi-ds"}
+        assert url_metadata.url == repo_url
 
 
 class TestDlregMetaExtract:
