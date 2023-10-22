d2cab252eea7c7361850050d20d7496ea3a3ea2b
Isaac To <candleindark@gmail.com>
2023-10-21 20:14:36 -0700

    Test the builtin_meta_extractors module falls back to `yaml.SafeLoader`

--- datalad_registry/tests/test_tasks/test_utils/test_builtin_meta_extractors.py
+++ datalad_registry/tests/test_tasks/test_utils/test_builtin_meta_extractors.py
@@ -1,11 +1,44 @@
+import importlib
+
 import pytest
 
-from datalad_registry.tasks.utils.builtin_meta_extractors import (
-    InvalidRequiredFileError,
-    dlreg_dandiset_meta_extract,
-    dlreg_meta_extract,
-)
 from datalad_registry.utils.datalad_tls import get_head_describe
+
+
+@pytest.fixture
+def load_builtin_meta_extractors_without_c_safe_loader(monkeypatch):
+    """
+    This fixture reloads the builtin meta extractors module without
+    the C-based YAML loader, `CSafeLoader`, in the `yaml` package.
+    """
+
+    with monkeypatch.context() as m:
+        import yaml
+
+        from datalad_registry.tasks.utils import builtin_meta_extractors
+
+        # Remove the C-based YAML loader, `CSafeLoader`, from the `yaml` package
+        m.delattr(yaml, "CSafeLoader", raising=False)
+
+        # Reload the builtin meta extractors module
+        importlib.reload(builtin_meta_extractors)
+
+        yield
+
+    # Reload the builtin meta extractors module
+    # in the original state of the `yaml` package
+    importlib.reload(builtin_meta_extractors)
+
+
+@pytest.mark.usefixtures("load_builtin_meta_extractors_without_c_safe_loader")
+def test_fallback_to_safe_loader():
+    """
+    Test that the builtin meta extractors module falls back to the Python-based YAML
+    loader, `SafeLoader`, when the C-based YAML loader, `CSafeLoader`, is not available
+    """
+    from datalad_registry.tasks.utils import builtin_meta_extractors
+
+    assert builtin_meta_extractors.SafeLoader.__name__ == "SafeLoader"
 
 
 class TestDlregDandisetMetaExtract:
@@ -14,6 +47,10 @@ class TestDlregDandisetMetaExtract:
         Test the case that the argument `url` is a valid `RepoUrl` object with a
         valid corresponding dandi dataset in the local cache
         """
+        from datalad_registry.tasks.utils.builtin_meta_extractors import (
+            dlreg_dandiset_meta_extract,
+        )
+
         repo_url = dandi_repo_url_with_up_to_date_clone[0]
         ds_clone = dandi_repo_url_with_up_to_date_clone[2]
 
@@ -32,6 +69,11 @@ class TestDlregDandisetMetaExtract:
         """
         Test the case that the `dandiset.yaml` file has no document
         """
+        from datalad_registry.tasks.utils.builtin_meta_extractors import (
+            InvalidRequiredFileError,
+            dlreg_dandiset_meta_extract,
+        )
+
         repo_url = dandi_repo_url_with_up_to_date_clone[0]
         ds_clone = dandi_repo_url_with_up_to_date_clone[2]
 
@@ -53,6 +95,10 @@ class TestDlregMetaExtract:
         """
         Test the case that the given extractor is not one that is supported
         """
+        from datalad_registry.tasks.utils.builtin_meta_extractors import (
+            dlreg_meta_extract,
+        )
+
         repo_url = dandi_repo_url_with_up_to_date_clone[0]
 
         with flask_app.app_context():
@@ -65,6 +111,10 @@ class TestDlregMetaExtract:
         """
         Test the case that the given extractor is one that is supported
         """
+        from datalad_registry.tasks.utils.builtin_meta_extractors import (
+            dlreg_meta_extract,
+        )
+
         repo_url = dandi_repo_url_with_up_to_date_clone[0]
 
         with flask_app.app_context():
