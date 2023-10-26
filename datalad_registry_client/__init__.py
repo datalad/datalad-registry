74bf6ec41a9a975505210665dce489fcf7aafef5
Isaac To <candleindark@gmail.com>
2023-10-26 12:23:05 -0700

    Revert "Enable datalad library mode in datalad-registry-client"
    This reverts commit d0d62707b49664c4a1285d422c3b1e53a44f5f77.

--- datalad_registry_client/__init__.py
+++ datalad_registry_client/__init__.py
@@ -1,12 +1,8 @@
 import sys
 
-import datalad
-
 # The default base API endpoint of the DataLad Registry instance
 # that the client interacts with.
 DEFAULT_BASE_ENDPOINT = "http://127.0.0.1:5000/api/v2"
-
-datalad.enable_librarymode()
 
 command_suite = (
     "Interact with DataLad registry",
