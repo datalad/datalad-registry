c46f48dcb1f95de3d78ea5bbb9a334c9c00b73df
Isaac To <candleindark@gmail.com>
2024-01-05 19:19:41 -0800

    Define `usage_dashboard.py` for definitions for accessing usage dashboard

---
+++ datalad_registry/tasks/utils/usage_dashboard.py
@@ -0,0 +1,9 @@
+# This file contains definitions for accessing information made available
+# by the datalad-usage-dashboard, https://github.com/datalad/datalad-usage-dashboard.
+
+
+# URL of the JSON representation of all the repositories in the datalad-usage-dashboard
+DASHBOARD_COLLECTION_URL = (
+    "https://raw.githubusercontent.com/datalad/"
+    "datalad-usage-dashboard/master/datalad-repos.json"
+)
