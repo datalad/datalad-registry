2751ba7b03cb3ebe3a05157c7bef4c30fae99dd3
Yaroslav Halchenko <debian@onerussian.com>
2024-02-21 13:55:05 -0500

    More testing, various minor tune ups, testing case insensitive
    Also now committed with fixup pre-commit so isort kicked in etc

--- datalad_registry/search.py
+++ datalad_registry/search.py
@@ -1,10 +1,19 @@
+"""Functionality for support of ad-hoc search "language".
+"""
+
 from functools import partial
+
 from lark import Lark, Token, Transformer, Tree, v_args
-from sqlalchemy import Text, or_, and_, not_
+from sqlalchemy import Text, and_, not_, or_
 
 from .models import RepoUrl, URLMetadata
 
+#
+# Constructs for the case-insensitive search via ilike
+#
 escape = "\\"
+
+
 def _escape_for_ilike(value):
     escaped_value = (
         value.replace(escape, escape + escape)
@@ -13,28 +22,36 @@ def _escape_for_ilike(value):
     )
     return f"%{escaped_value}%"
 
-# ATM most are  1:1 mapping to DB schema.
+
+# ATM most of our search targets are  1:1 mapping to DB schema.
 # List explicitly so we could reuse in grammar and error messages etc
 # without duplication
 known_fields_RepoUrl_1to1 = ["url", "ds_id", "head", "head_describe", "tags"]
+
+
 def get_ilike_search(model, field, value):
     return getattr(model, field).ilike(_escape_for_ilike(value), escape=escape)
+
 
 def get_metadata_ilike_search(value):
     escaped_value = _escape_for_ilike(value)
     return RepoUrl.metadata_.any(
         or_(
             URLMetadata.extractor_name.ilike(escaped_value, escape=escape),
-            URLMetadata.extracted_metadata.cast(Text).ilike(escaped_value, escape=escape),
+            URLMetadata.extracted_metadata.cast(Text).ilike(
+                escaped_value, escape=escape
+            ),
         )
     )
+
+
 def get_branches_ilike_search(value):
     return RepoUrl.branches.cast(Text).ilike(_escape_for_ilike(value), escape=escape)
 
+
 # mapping to schema of fields
 known_fields = {
-    _: partial(get_ilike_search, RepoUrl, _)
-    for _ in known_fields_RepoUrl_1to1
+    _: partial(get_ilike_search, RepoUrl, _) for _ in known_fields_RepoUrl_1to1
 }
 known_fields["metadata"] = get_metadata_ilike_search
 known_fields["branches"] = get_branches_ilike_search
@@ -42,9 +59,12 @@ known_fields["branches"] = get_branches_ilike_search
 # TODO: add "metadata_extractor"?
 known_fields_str = "|".join(f'"{_}"' for _ in known_fields)
 
-# This is a grammar for the search query language.
-# For interactive testing, you can use the Lark parser at https://www.lark-parser.org/ide/
-grammar = fr"""
+#
+# This is a lark grammar for the search query language.
+# For interactive testing, you can use the Lark parser at
+# https://www.lark-parser.org/ide/ .
+#
+grammar = rf"""
 ?start: search
 ?search: orand_exp
 ?orand_exp: not_exp
@@ -69,7 +89,8 @@ grammar = fr"""
 ?metadata_extractors: WORD (","WORD)*
 !field: {known_fields_str}
 !metadata_field: "metadata"
-// To make it easier for user to be informed about an unknown field being specified in the field_matching
+// To make it easier for user to be informed about an unknown field being \
+// specified in the field_matching
 !unknown_field_error: WORD
 
 // Let's make it into a rule so we could have transformer for it
@@ -81,21 +102,33 @@ WORD: /[-_.*?+\w]+/
 !op: "?"   // string present somewhere within
     |"="   // equal to the value as a string
     |">"   // greater to the value as a string
-    |"~"   // regex search, should be able to use ^ and $ to anchor if desired 
+    |"~"   // regex search, should be able to use ^ and $ to anchor if desired
 // TODO:
-//  - come up with transformers, e.g. (int) or `!int` to precede :? etc which should change how operations like =, >
+//  - come up with transformers, e.g. (int) or `!int` to precede :? etc which
+//    should change how operations like =, >
 //  - operator "in" might come handy
-//  - also to make it case-sensitive since to make most useful - by default are case insensitive!
+//  - also to make it case-sensitive since to make most useful - by default are\
+//    case insensitive!
 
 %import common.ESCAPED_STRING
 %import common.WS
 %ignore WS
 """
-with open("/tmp/grammar.lark", "w") as f:
-    f.write(grammar)
 
-@v_args(inline=True)    # Affects the signatures of the methods
+
+def _dump_grammar():
+    # Could be used for debugging on the web -- the schema with those fields embedded
+    with open("/tmp/grammar.lark", "w") as f:
+        f.write(grammar)
+
+
+# _dump_grammar()
+
+
+@v_args(inline=True)  # Affects the signatures of the methods
 class SearchQueryTransformer(Transformer):
+    """Convert the parsed search query into SQLAlchemy expressions."""
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Additional initialization here if needed
@@ -112,29 +145,26 @@ class SearchQueryTransformer(Transformer):
         # args will be a single-element list containing the NOT argument
         return not_(arg)
 
-    ## ?secondary: (field_select | unknown_field_error) ":" op? (quoted_string | WORD) -> field_matched
     def get_field_select_search(self, metadata_field_l, metadata_extractors_l, value):
-        assert metadata_field_l.data.value == 'metadata_field'
+        assert metadata_field_l.data.value == "metadata_field"
         if isinstance(metadata_extractors_l, Token):
             extractors = [metadata_extractors_l.value]
         else:  # was more than one
             assert isinstance(metadata_extractors_l, Tree)
-            assert metadata_extractors_l.data.value == 'metadata_extractors'
-            extractors = list(map(self._get_str_value,
-                                  metadata_extractors_l.children))
+            assert metadata_extractors_l.data.value == "metadata_extractors"
+            extractors = list(map(self._get_str_value, metadata_extractors_l.children))
         if not extractors:
             raise ValueError(f"No extractors were specified in {metadata_extractors_l}")
+        # TODO: might want to provide a dedicated simpler one
         # elif len(extractors) == 1:
-        #     raise NotImplementedError(f"Single extractor {extractors} is not implemented")
+        #     raise NotImplementedError(f"Single extractor {extractors}")
         else:
             # ??? it seems we do not have search target value here, so we are to
             # return the function to search with but we can't since here we already
             # need to know ilike vs exact match
             return RepoUrl.metadata_.any(
                 and_(
-                    or_(
-                        *(URLMetadata.extractor_name == _ for _ in extractors)
-                    ),
+                    or_(*(URLMetadata.extractor_name == _ for _ in extractors)),
                     # search the entire JSON column as text
                     URLMetadata.extracted_metadata.cast(Text).icontains(
                         value, autoescape=True
@@ -147,18 +177,18 @@ class SearchQueryTransformer(Transformer):
         # logic based on your specific fields and operations.
         if len(args) == 3:
             field_l, op_l, value_l = args
-            assert op_l.data.value == 'op'
+            assert op_l.data.value == "op"
             op = op_l.children[0].value
         elif len(args) == 2:
             # ":" is identical to ":?
             field_l, value_l = args
-            op = '?'
+            op = "?"
         else:
             raise ValueError(f"Unexpected number of args: {len(args)} in {args}")
 
-        if field_l.data == 'field_select':
+        if field_l.data == "field_select":
             search = partial(self.get_field_select_search, *field_l.children)
-        elif field_l.data.value == 'field':
+        elif field_l.data.value == "field":
             assert len(field_l.children) == 1  # TODO: handle multiple??
             field = field_l.children[0].value
             search = known_fields[field]
@@ -173,10 +203,10 @@ class SearchQueryTransformer(Transformer):
     def _get_str_value(self, arg: Token):
         assert isinstance(arg, Token)
         value = arg.value
-        if arg.type == 'WORD':
+        if arg.type == "WORD":
             return value
-        elif arg.type == 'ESCAPED_STRING':
-            return value[1:-1].replace(r'\"', '"')
+        elif arg.type == "ESCAPED_STRING":
+            return value[1:-1].replace(r"\"", '"')
         else:
             raise TypeError(arg.type)
 
@@ -189,11 +219,12 @@ class SearchQueryTransformer(Transformer):
 
     def unknown_field_error(self, arg):
         # Handle unknown fields
-        raise ValueError(f"Unknown field: '{arg}'. Known are: {', '.join(known_fields)}")
+        raise ValueError(
+            f"Unknown field: '{arg}'. Known are: {', '.join(known_fields)}"
+        )
 
 
 # Assuming `grammar` is your grammar definition string
-parser = Lark(grammar, parser='lalr', transformer=SearchQueryTransformer())
-
-# `query_struct` now is an SQLAlchemy expression ready to be applied to a query
-## r = session.query(RepoUrl).filter(query_struct)
+parser = Lark(grammar, parser="lalr", transformer=SearchQueryTransformer())
+# Example:
+# r = session.query(RepoUrl).filter(parser.parse("url:example OR ds_id:handbook"))
--- datalad_registry/tests/test_search.py
+++ datalad_registry/tests/test_search.py
@@ -1,12 +1,14 @@
 import pytest
 
-from datalad_registry.models import RepoUrl, db, URLMetadata
+from datalad_registry.models import RepoUrl, URLMetadata, db
+
 from ..search import parser
 
 
 def test_search_errors():
     with pytest.raises(ValueError):
         parser.parse("unknown_field:example")
+
 
 @pytest.fixture
 def populate_with_url_metadata_for_search(
@@ -51,6 +53,7 @@ def populate_with_url_metadata_for_search(
             db.session.add(metadata)
         db.session.commit()
 
+
 @pytest.mark.usefixtures("populate_with_url_metadata_for_search")
 @pytest.mark.parametrize(
     "query, expected",
@@ -58,26 +61,35 @@ def populate_with_url_metadata_for_search(
         # based purely on url field
         ("example", [1]),
         ("example OR handbook", [1, 3]),
+        # case insensitive
+        ("Example OR handBook", [1, 3]),
         ("example AND handbook", []),
         ("datalad OR handbook", [2, 3]),
         ("datalad AND handbook", [3]),
-        ("handbook datalad", [3]),        # should be identical result to above AND
+        ("handbook datalad", [3]),  # should be identical result to above AND
         ("handbook url:datalad", [3]),
         ("handbook ds_id:datalad", []),
         ("handbook OR ds_id:datalad", [3]),
         ("url:handbook OR ds_id:datalad", [3]),
         ("url:handbook OR ds_id:844c", [1, 2, 3]),
         ("(url:handbook OR metadata[metalad_core]:meta1value) AND ds_id:844c", [2, 3]),
-        ("(url:handbook OR metadata[metalad_core]:meta3value) AND ds_id:844c", [1, 3]),
+        ("(url:handbook OR metadata[metalad_core]:meta3value) AND ds_id:844C", [1, 3]),
         ("(url:handbook OR metadata[metalad_core]:value) AND ds_id:844c", [1, 2, 3]),
-
-        ("example AND NOT another", []),
-        ("example AND (another OR test)", []),
-        ("example AND (another OR test) AND NOT (example OR another)", []),
-        ("example AND (another OR test) AND NOT (example OR another) AND another", []),
-        ("metadata[ex1,ex2]:\"specific data\"", []),
-        (r"""((jim AND NOT haxby AND "important\" paper") OR ds_id:"^000[3-9]..$" OR url:"example.com") AND metadata:non""", []),
-        (r"""((jim AND NOT haxby AND "important\" paper") OR ds_id:"^000[3-9]..$" OR url:"example.com") AND metadata:non AND metadata[ex1,ex2]:"specific data" AND metadata[extractor2]:data""", []),
+        ("meta2value", [2]),
+        ('metadata:"value"', [1, 2]),
+        ('metadata[metalad_studyminimeta]:"value"', []),
+        # ATM only exact match for extractor
+        ('metadata[metalad]:"value"', []),
+        ('metadata[metalad_core]:"value"', [1, 2]),
+        # OR among multiple listed, ok to have unknown
+        ('metadata[metalad_core,metalad_studyminimeta,unknown]:"value"', [1, 2]),
+        # Prototypical query for which we do not have full support yet, e.g.
+        # regex matching :~
+        #  (r"""((jim AND NOT haxby AND "important\" paper") OR ds_id:~"^000[3-9]..$"
+        #   OR url:"example.com") AND metadata:non AND metadata[ex1,ex2]:"specific data"
+        #   AND metadata[extractor2]:data""", []),
+        # Find datasets with the last
+        #  (metadata[bids_dataset][Authors][-1]:haxby ...)
     ],
 )
 def test_search_complex_smoke(flask_app, query, expected):
@@ -86,5 +98,5 @@ def test_search_complex_smoke(flask_app, query, expected):
     with flask_app.app_context():
         result = db.session.query(RepoUrl).filter(r)
         hits = [_.id for _ in result.all()]
-        print(expected, hits)
+        # print(expected, hits)
         assert hits == expected
