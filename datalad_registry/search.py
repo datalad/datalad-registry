b3079b632f4ee0152e63fdd8bf34b0d91101c71b
Yaroslav Halchenko <debian@onerussian.com>
2024-02-20 21:08:28 -0500

    WiP: started to hang on the parsed content the sqlalchemy queries

--- datalad_registry/search.py
+++ datalad_registry/search.py
@@ -1,12 +1,41 @@
+from functools import partial
 from lark import Lark, Token, Transformer, v_args
-from sqlalchemy import or_, and_, not_
+from sqlalchemy import Text, or_, and_, not_
 
-from .models import RepoUrl
+from .models import RepoUrl, URLMetadata
 
-# ATM 1:1 mapping to DB schema.
+escape = "\\"
+def _escape_for_ilike(value):
+    escaped_value = (
+        value.replace(escape, escape + escape)
+        .replace("%", escape + "%")
+        .replace("_", escape + "_")
+    )
+    return f"%{escaped_value}%"
+
+# ATM most are  1:1 mapping to DB schema.
 # List explicitly so we could reuse in grammar and error messages etc
 # without duplication
-known_fields = ["url", "ds_id", "head", "head_describe", "branches", "tags", "metadata"]
+known_fields_RepoUrl_1to1 = ["url", "ds_id", "head", "head_describe", "branches", "tags"]
+def get_ilike_search(model, field, value):
+    return getattr(model, field).ilike(_escape_for_ilike(value), escape=escape)
+
+def get_metadata_ilike_search(value):
+    escaped_value = _escape_for_ilike(value)
+    return RepoUrl.metadata_.any(
+        or_(
+            URLMetadata.extractor_name.ilike(escaped_value, escape=escape),
+            URLMetadata.extracted_metadata.cast(Text).ilike(escaped_value, escape=escape),
+        )
+    )
+
+# mapping to schema of fields
+known_fields = {
+    _: partial(get_ilike_search, RepoUrl, _)
+    for _ in known_fields_RepoUrl_1to1
+}
+known_fields["metadata"] = get_metadata_ilike_search
+# TODO: add "metadata_extractor"?
 known_fields_str = "|".join(f'"{_}"' for _ in known_fields)
 
 # This is a grammar for the search query language.
@@ -31,7 +60,8 @@ grammar = fr"""
 // later to subselect metadata
 // ?field_select: (field | metadata_field "[" metadata_extractors "]") value_path?
 // ?value_path: ("[" (WORD|quoted_string) "]")+
-?field_select: (field | metadata_field "[" metadata_extractors "]")
+?field_select: field
+    | metadata_field "[" metadata_extractors "]" -> field_select
 ?metadata_extractors: WORD (","WORD)*
 !field: {known_fields_str}
 !metadata_field: "metadata"
@@ -40,7 +70,7 @@ grammar = fr"""
 
 // Let's make it into a rule so we could have transformer for it
 // and unescape escaped \s
-!quoted_string: ESCAPED_STRING // /"((?:\.|[^\"])*)"/
+?quoted_string: ESCAPED_STRING // /"((?:\.|[^\"])*)"/
 WORD: /[-_.*?+\w]+/
 
 // :operations to aim for
@@ -57,7 +87,8 @@ WORD: /[-_.*?+\w]+/
 %import common.WS
 %ignore WS
 """
-
+with open("/tmp/grammar.lark", "w") as f:
+    f.write(grammar)
 
 @v_args(inline=True)    # Affects the signatures of the methods
 class SearchQueryTransformer(Transformer):
@@ -65,11 +96,11 @@ class SearchQueryTransformer(Transformer):
         super().__init__(*args, **kwargs)
         # Additional initialization here if needed
 
-    def or_search(self, args):
+    def or_search(self, *args):
         # args will be a list of the arguments to the OR operation
         return or_(*args)
 
-    def and_search(self, args):
+    def and_search(self, *args):
         # args will be a list of the arguments to the AND operation
         return and_(*args)
 
@@ -77,43 +108,70 @@ class SearchQueryTransformer(Transformer):
         # args will be a single-element list containing the NOT argument
         return not_(arg)
 
-    def field_matched(self, args):
+    ## ?secondary: (field_select | unknown_field_error) ":" op? (quoted_string | WORD) -> field_matched
+    def __field_select(self, metadata_field_l, metadata_extractors_l):
+        assert metadata_field_l.data.value == 'metadata_field'
+        assert metadata_extractors_l.data.value == 'metadata_extractors'
+
+        extractors = list(map(self._get_str_value,
+                              metadata_extractors_l.children))
+        if not extractors:
+            raise ValueError(f"No extractors were specified in {metadata_extractors_l}")
+        elif len(extractors) == 1:
+            raise NotImplementedError(f"Single extractor {extractors} is not implemented")
+        else:
+            # ??? it seems we do not have search target value here, so we are to
+            # return the function to search with but we can't since here we already
+            # need to know ilike vs exact match
+            return RepoUrl.metadata_.any(
+                or_(
+                    URLMetadata.extractor_name == extractor,
+                    # search the entire JSON column as text
+                    URLMetadata.extracted_metadata.cast(Text).icontains(
+                        value, autoescape=True
+                    ),
+                )
+            )
+
+    def field_matched(self, *args):
         # Example for handling field-specific matches. You'll need to expand this
         # logic based on your specific fields and operations.
-        field, op, value = args
-        import pdb; pdb.set_trace()
-        if op == "=":
-            op_func = lambda field, value: field == value
-        elif op == "?":
-            op_func = lambda field, value: field.ilike(f"%{value}%")
-        # Add more operations as necessary
-
-        # Mapping from field names to RepoUrl attributes
-        field_map = {
-            "url": RepoUrl.url,
-            "ds_id": RepoUrl.ds_id,
-            # Add more mappings as necessary
-        }
-
-        if field in field_map:
-            return op_func(field_map[field], value)
+        if len(args) == 3:
+            field_l, op_l, value_l = args
+            assert op_l.data.value == 'op'
+            op = op_l.children[0].value
+        elif len(args) == 2:
+            # ":" is identical to ":?
+            field_l, value_l = args
+            op = '?'
         else:
-            # Handle unknown fields, possibly raise an error or ignore
-            pass
+            raise ValueError(f"Unexpected number of args: {len(args)} in {args}")
 
-    def search_word(self, arg: Token):
-        # Handle generic search words
-        import pdb; pdb.set_trace()
-        word = args[0]
-        return or_(
-            RepoUrl.url.ilike(f"%{word}%"),
-            RepoUrl.ds_id.ilike(f"%{word}%"),
-            # Add more fields as needed
-        )
+        assert field_l.data.value == 'field'
+        assert len(field_l.children) == 1  # TODO: handle multiple??
+        field = field_l.children[0].value
 
-    def search_string(self, args):
-        # Similar to search_word, but for quoted strings
-        return self.search_word(args)
+        if op == "?":
+            return known_fields[field](self._get_str_value(value_l))
+        else:
+            raise NotImplementedError(f"Operation {op} is not implemented")
+
+    def _get_str_value(self, arg: Token):
+        assert isinstance(arg, Token)
+        value = arg.value
+        if arg.type == 'WORD':
+            return value
+        elif arg.type == 'ESCAPED_STRING':
+            return value[1:-1].replace(r'\"', '"')
+        else:
+            raise TypeError(arg.type)
+
+    def _get_str_search(self, arg: Token):
+        value = self._get_str_value(arg)
+        return or_(*[f(value) for f in known_fields.values()])
+
+    search_word = _get_str_search
+    search_string = _get_str_search
 
     def unknown_field_error(self, arg):
         # Handle unknown fields
--- datalad_registry/tests/test_search.py
+++ datalad_registry/tests/test_search.py
@@ -4,11 +4,25 @@ from ..search import parser
 
 
 def test_search_errors():
-    with pytest.assert_raises(ValueError):
+    with pytest.raises(ValueError):
         parser.parse("unknown_field:example")
 
 
-def test_search_complex():
-    query = r"""((jim AND NOT haxby AND "important\" paper") OR ds_id:~"^000[3-9]..$" OR url:"example.com") AND metadata:non AND metadata[ex1,ex2]:"specific data" AND metadata[extractor2]:data"""
+@pytest.mark.parametrize(
+    "query",
+    [
+        "example",
+        "example OR another",
+        "example AND another",
+        "example AND NOT another",
+        "example AND (another OR test)",
+        "example AND (another OR test) AND NOT (example OR another)",
+        "example AND (another OR test) AND NOT (example OR another) AND another",
+        r"""((jim AND NOT haxby AND "important\" paper") OR ds_id:"^000[3-9]..$" OR url:"example.com") AND metadata:non""",
+        # not yet supported -- need to figure out how to handle metadata fields
+        #"""((jim AND NOT haxby AND "important\" paper") OR ds_id:"^000[3-9]..$" OR url:"example.com") AND metadata:non AND metadata[ex1,ex2]:"specific data" AND metadata[extractor2]:data""",
+],
+)
+def test_search_complex_smoke(query):
     r = parser.parse(query)
-    print(r)
+    print(f"QUERY {query}: {r}")
