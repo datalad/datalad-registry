7c94133e7d0bd7d078f2fc64be9af7ff53889b67
Isaac To <candleindark@gmail.com>
2024-03-06 16:52:51 -0800

    Remove invocation in a subshell
    A separate shell is already used to execute the script

--- tools/service.sh
+++ tools/service.sh
@@ -9,4 +9,4 @@ umask 077
 # Set working directory to the root of the Datalad-Registry repository
 cd $(dirname $0) && cd ..
 
-(set -a && . ./.env.typhon && set +a && podman-compose -f docker-compose.yml up -d --build)
+set -a && . ./.env.typhon && set +a && podman-compose -f docker-compose.yml up -d --build
