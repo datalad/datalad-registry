80f79a2723312faf4509524624d8a34771046a2c
Isaac To <candleindark@gmail.com>
2024-03-06 17:20:17 -0800

    Specify the loading of env var in a block instead
    This is more legible in comparison to a chain of `&&`

--- tools/service.sh
+++ tools/service.sh
@@ -9,4 +9,9 @@ umask 077
 # Set working directory to the root of the Datalad-Registry repository
 cd $(dirname $0) && cd ..
 
-set -a && . ./.env.typhon && set +a && podman-compose -f docker-compose.yml up -d --build
+# Load environment variables from .env.typhon
+set -a
+source ./.env.typhon
+set +a
+
+podman-compose -f docker-compose.yml up -d --build
