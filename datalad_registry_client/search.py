8e524280abc9eb7c789e1c669dbed49899478f0b
Isaac To <candleindark@gmail.com>
2024-03-21 12:43:25 -0700

    Add template code for Parameters

--- datalad_registry_client/search.py
+++ datalad_registry_client/search.py
@@ -8,8 +8,10 @@ from os.path import abspath, curdir
 from datalad.distribution.dataset import datasetmethod
 from datalad.interface.base import Interface, build_doc, eval_results
 from datalad.interface.results import get_status_dict
-from datalad.support.constraints import EnsureChoice
+from datalad.support.constraints import EnsureNone, EnsureStr
 from datalad.support.param import Parameter
+
+from . import DEFAULT_BASE_ENDPOINT
 
 lgr = logging.getLogger("datalad.registry.search")
 
@@ -28,14 +30,24 @@ class RegistrySearch(Interface):
     # parameters of the command, must be exhaustive
     _params_ = dict(
         # name of the parameter, must match argument name
-        language=Parameter(
+        search_str=Parameter(
             # cmdline argument definitions, incl aliases
-            args=("-l", "--language"),
+            args=("-s", "--search-str"),
             # documentation
-            doc="""language to say "hello" in""",
+            doc="""The search string used to perform a search identical to the one
+            offered by the Web UI. Please consult the Web UI for the expected syntax of
+            this search string by clicking on the "Show Search Query Syntax" button.""",
             # type checkers, constraint definition is automatically
             # added to the docstring
-            constraints=EnsureChoice("en", "de"),
+            constraints=EnsureStr(),
+            required=True,
+        ),
+        base_endpoint=Parameter(
+            args=("-e", "--base-endpoint"),
+            doc=f"""The base API endpoint of the DataLad Registry instance to interact
+            with. This defaults to the datalad_registry.base_endpoint option if set,
+            or {DEFAULT_BASE_ENDPOINT} otherwise.""",
+            constraints=EnsureStr() | EnsureNone(),
         ),
     )
 
