17b786b641533c2b792bd9ad33e3598320194129
Isaac To <candleindark@gmail.com>
2024-05-08 17:34:09 -0700

    Use `CTE` instead of `subquery` in `get_dl_ds_collection_stats_with_dups`
    The result of `CTE` is reused in a query, so performance is increased

--- datalad_registry/blueprints/api/dataset_urls/tools.py
+++ datalad_registry/blueprints/api/dataset_urls/tools.py
@@ -122,21 +122,21 @@ def get_unique_dl_ds_collection_stats(base_cte: CTE) -> AnnexDsCollectionStats:
     return _get_annex_ds_collection_stats(grp_by_id_and_a_f_size_q)
 
 
-def get_dl_ds_collection_stats_with_dups(base_q: Subquery) -> AnnexDsCollectionStats:
+def get_dl_ds_collection_stats_with_dups(base_cte: CTE) -> AnnexDsCollectionStats:
     """
     Get the stats of the subset of the collection of datasets that contains only
     of Datalad datasets, considering individual repos as a dataset regardless of
     the value of `ds_id`.
 
-    :param base_q: The base query that specified the collection of datasets
-                   under consideration
+    :param base_cte: The base CTE that specified the collection of datasets
+                     under consideration
     :return: The object representing the stats
 
     Note: The execution of this function requires the Flask app's context
     """
 
     # Select statement for getting all the Datalad datasets
-    dl_ds_q = select(base_q).filter(base_q.c.ds_id.is_not(None)).subquery("dl_ds_q")
+    dl_ds_q = select(base_cte).filter(base_cte.c.ds_id.is_not(None)).subquery("dl_ds_q")
 
     return _get_annex_ds_collection_stats(dl_ds_q)
 
